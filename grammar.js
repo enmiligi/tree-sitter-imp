740bbeccccac3345a50e5810dc6e09a17540e9a2
Enea Giger <enea.giger@tuta.io>
2025-05-26 17:56:47 +0200

    feat: all expressions

--- grammar.js
+++ grammar.js
@@ -16,7 +16,15 @@ export default grammar({
 
   rules: {
     source_file: ($) =>
-      seq(optional($._expr), repeat(seq($._newStatement, optional($._expr)))),
+      seq(
+        optional($._statement),
+        repeat(seq($._newStatement, optional($._statement))),
+      ),
+
+    _statement: ($) => choice($.letStatement),
+
+    letStatement: ($) =>
+      seq("let", field("name", $.identifier), "=", field("value", $._expr)),
 
     _expr: ($) =>
       choice(
@@ -26,21 +34,89 @@ export default grammar({
         $.charLiteral,
         $.stringLiteral,
         $.call,
+        $._parenExpr,
+        $.letExpr,
+        $.lambda,
+        $.if,
+        $.list,
+        $.not,
+        $.add,
+        $.subtract,
+        $.multiply,
+        $.divide,
+        $.lessThan,
+        $.moreThan,
+        $.equals,
+        $.notEquals,
+        $.seq,
       ),
 
-    call: ($) => prec.left(seq($._expr, $._expr)),
+    not: ($) => seq("!", $._expr),
+
+    add: ($) => prec.left(20, seq($._expr, "+", $._expr)),
+    subtract: ($) => prec.left(20, seq($._expr, "-", $._expr)),
+    multiply: ($) => prec.left(30, seq($._expr, "*", $._expr)),
+    divide: ($) => prec.left(30, seq($._expr, "/", $._expr)),
+
+    lessThan: ($) => prec.left(10, seq($._expr, "<", $._expr)),
+    moreThan: ($) => prec.left(10, seq($._expr, ">", $._expr)),
+    equals: ($) => prec.left(10, seq($._expr, "==", $._expr)),
+    notEquals: ($) => prec.left(10, seq($._expr, "!=", $._expr)),
+
+    seq: ($) => prec.left(5, seq($._expr, ";", $._expr)),
+
+    call: ($) => prec.left(100, seq($._expr, $._expr)),
+
+    _parenExpr: ($) => seq("(", $._expr, ")"),
+
+    letExpr: ($) =>
+      seq(
+        "let",
+        field("name", $.identifier),
+        "=",
+        field("value", $._expr),
+        "in",
+        field("result", $._expr),
+      ),
+
+    lambda: ($) =>
+      seq(
+        "lambda",
+        field("argument", $.identifier),
+        ".",
+        field("body", $._expr),
+      ),
+
+    if: ($) =>
+      seq(
+        "if",
+        field("predicate", $._expr),
+        "then",
+        field("then", $._expr),
+        "else",
+        field("else", $._expr),
+      ),
+
+    list: ($) =>
+      seq(
+        "[",
+        optional(seq($._expr, repeat(seq(",", $._expr)), optional(","))),
+        "]",
+      ),
+
+    boolLiteral: ($) => choice("True", "False"),
 
     intLiteral: ($) => token(/\d+/),
     floatLiteral: ($) => token(/\d+\.\d*/),
 
-    charLiteral: ($) => seq("'", $.char, "'"),
-    stringLiteral: ($) => seq('"', repeat($.char), '"'),
+    charLiteral: ($) => seq("'", $._char, "'"),
+    stringLiteral: ($) => seq('"', repeat($._char), '"'),
 
     _char: ($) => /[^\\]|\\(n|r|t|'|"|\\)/,
 
     identifier: ($) => token(/[A-Za-z]\w*/),
 
-    _SPACE: ($) => token(/(\s+|\s*\n[\r\t\f\v ]+)/),
+    _SPACE: ($) => token(/([\t\f\v ]+|(\n|\r|\r\n)[\t\f\v ]+)/),
 
     _newStatement: ($) => token("\n"),
   },
